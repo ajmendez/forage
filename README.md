f5b307d9a9f111a4f74fbe59bebd1a2123ae895e
Alexander Mendez <bluespace@gmail.com>
2014-06-19 17:07:42 -0700

    backups are huge.  TODO: compression

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ forage
 Forage fondles Feedly.
 
 
-https://gist.github.com/d3m3vilurr/5904029
-https://github.com/zgw21cn/FeedlyClient
-http://developer.feedly.com/v3/
+* https://gist.github.com/d3m3vilurr/5904029
+* https://github.com/zgw21cn/FeedlyClient
+* http://developer.feedly.com/v3/
--- forage/api.py
+++ forage/api.py
@@ -1,4 +1,6 @@
 # api -- some nice api like functions.
+import os
+import json
 import client
 import requests
 import numpy as np
@@ -7,6 +9,11 @@ from datetime import datetime
 
 RATELIMIT = 200 # really this is 250 -- but lets not anger the gods.
 URL = 'http://cloud.feedly.com/v3/'
+
+USERFILE = os.path.expanduser('~/data/feedly/user.json')
+READFILE = os.path.expanduser('~/data/feedly/read.json')
+SAVEDFILE = os.path.expanduser('~/data/feedly/saved.json')
+
 
 try:
     from pymendez import auth
@@ -23,10 +30,41 @@ def convert_time(timestamp):
 
 
 
-class Data(object):
-  def __init__(self, filename):
-    self.filename = filename
 
+class Data(object):
+  def __init__(self, filename, fcn=list):
+    self.filename = filename
+    self.fcn = fcn
+    
+  def __enter__(self, *args, **kwargs):
+    try:
+      self.data = self.load()
+    except:
+      print ' Failed to load: {}'.format(self.filename)
+      isok = raw_input('Are you ok with this? [yes/no]: ')
+      if 'y' in isok.lower():
+        self.data = self.fcn()
+      else:
+        raise
+    return self
+  
+  def __exit__(self, *args, **kwargs):
+    json.dump(self.data, 
+              open(self.filename,'w'),
+              indent=2)
+  
+  def __len__(self):
+    return len(self.data)
+  
+  def load(self):
+    return json.load(open(self.filename))
+  
+  def append(self, item):
+    self.data.append(item)
+  
+  def get(self, key):
+    return [item[key] for item in self.data]
+      
 
 
 class Forage(object):
@@ -34,13 +72,15 @@ class Forage(object):
     self.userid = userid
     self.token = token
     self.ratelimit = 0
+    self.headers = {}
     self.api = client.FeedlyClient(access_token=token, sandbox=False)
-  
+    
   
   def _get(self, url, **kwargs):
     '''Get the json of a url with some keywords
     keywords are passed into the requests.get command'''
     if self.ratelimit > RATELIMIT:
+      pprint(self.headers)
       raise IOError('Rate Limited!')
     tmp = {'headers':{'Authorization':'OAuth {}'.format(self.token)}}
     tmp.update(kwargs)
@@ -48,8 +88,10 @@ class Forage(object):
     
     # try to keep abreast of the rate limit
     try:
+      self.headers = response.headers
       self.ratelimit = int(response.headers['x-ratelimit-count'])
     except:
+      print self.headers
       pass
     return response.json()
   
@@ -70,7 +112,7 @@ class Forage(object):
     '''Get the response json for a streamid.  extra args are 
     passed in a params.'''
     url = 'streams/contents'
-    params = {'streamId':streamid, 'count':1000}
+    params = {'streamId':streamid, 'count':10000}
     params.update(kwargs)
     return self._get(url, params=params)
   
@@ -136,7 +178,7 @@ if __name__ == '__main__':
       k = len(uids)
       if (k%10000) == 0:
         print k, len(np.unique(uids)), api.ratelimit
-        print convert_time(event['actionTimestamp'])
+        print convert_time(entry['actionTimestamp'])
         pprint(entry)
     
     
